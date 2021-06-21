462be73b9b358fae6b6aecc4a71509ffaf75d9cb
Shadowstar <68189386+ShadowstarUnited@users.noreply.github.com>
2021-06-21 16:05:57 +0100

    Delete changelog directory

--- changelog/js/main.js
+++
@@ -1,8 +0,0 @@
-function toggleNavigation() {
-    var x = document.getElementById("navbar");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
