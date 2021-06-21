97f63dde998255bfb16bac03f6e41d740c46eccb
Shadowstar <68189386+ShadowstarUnited@users.noreply.github.com>
2021-06-21 15:01:52 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function toggleNavigation() {
+    var x = document.getElementById("navbar");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    } 
+}
